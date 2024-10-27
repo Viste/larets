1406a64d3cb708ff97eacd61b1db2f02c9d744cf
viste <viste02@gmail.com>
2024-10-27 10:56:49 +0300

    fix main fo gorm
    Signed-off-by: viste <viste02@gmail.com>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,29 +4,26 @@ import (
 	"github.com/Viste/larets/api"
 	"github.com/Viste/larets/config"
 	"github.com/Viste/larets/db"
+	"github.com/Viste/larets/repositories"
 	"log"
 	"os"
 )
 
 func main() {
-	// Загрузка конфигурации
 	config.LoadConfig()
 
-	// Чтение конфигурации из переменных окружения
 	connStr := os.Getenv("DATABASE_URL")
 	if connStr == "" {
 		log.Fatal("Не задана переменная окружения DATABASE_URL")
 	}
 
-	// Инициализация базы данных
-	database, err := db.InitDB(connStr)
+	err := db.InitDB(connStr)
 	if err != nil {
 		log.Fatal("Ошибка при инициализации базы данных: ", err)
 	}
-	defer database.Close()
 
 	log.Println("База данных успешно инициализирована")
 
-	// Запуск API сервера
+	repositories.InitRepositories()
 	api.RunAPIServer()
 }
